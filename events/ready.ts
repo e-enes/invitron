d1fcaa071060640bd3bc654829f76d3b81189657
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:09:50 +0200

    Update ready.ts

--- events/ready.ts
+++ events/ready.ts
@@ -1,10 +1,13 @@
+import {Events} from "discord.js";
 import MyClient from "../ts/class/MyClient";
 import {register} from "../utils/slashsync";
+import channelsync from "../utils/channelsync";
 import connection from "../data/connection";
+import config from "../config";
 
 export default {
     once: true,
-    name: "ready",
+    name: Events.ClientReady,
     async execute(client: MyClient) {
         await register(client,
             client.register.map((command) => ({
@@ -16,14 +19,18 @@ export default {
         );
         await connection.executeQueries();
 
-        client.user!.setActivity("Sene Bot");
+        client.user!.setActivity({name: config.activity.name, type: config.activity.type});
 
         for (let i = 0; i < client.guilds.cache.size; i++) {
             const guild = client.guilds.cache.at(i)!;
+
+            const channels = await channelsync.getChannels(guild.id);
+            client.cache.set(guild.id, channels);
+
             const guildInvites = await guild.invites.fetch();
             const code = new Map();
             guildInvites.each((inv) => code.set(inv.code, inv.uses));
             client.invites.set(guild.id, code);
         }
     }
-};
+}
