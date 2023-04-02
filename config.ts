3b49d0d8e78ac3683a45664e8b77aa1a58e72e02
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:01:00 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,10 @@
 export default {
-    channel: {
-        welcome: "",
-        leave: "",
-        log: ""
-    }
+    message: {
+        footer: "Powered by Enes"
+    },
+    activity: {
+        name: "Sene Bot",
+        type: 3 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom, 5 = Competing
+    },
+    handleError: false // If there an error, display it in the message
 }
