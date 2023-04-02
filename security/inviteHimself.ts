e4da2860c07bd7ff296b930b8e05708eb3c92a73
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:05:43 +0200

    Update inviteHimself.ts

--- security/inviteHimself.ts
+++ security/inviteHimself.ts
@@ -1,19 +1,21 @@
-import {EmbedBuilder, GuildMember, TextChannel} from "discord.js";
+import {EmbedBuilder, GuildMember, GuildTextBasedChannel, TextChannel} from "discord.js";
 import MyClient from "../ts/class/MyClient";
+import config from "../config";
+import channelsync from "../utils/channelsync";
 
 export default async function antiBot(member: GuildMember, client: MyClient, joined: boolean) {
-    const embed = new EmbedBuilder()
-        .setTitle(joined ? `${member.user.tag} joined!` : `${member.user.tag} left!`)
-        .setDescription(
-            `
-            **Invited by**: Himself\n
-            **Account create**: <t:${Math.floor(member.user.createdTimestamp) / 1000}:R>\n\n
-            _His invitation is invalid!_
-            `
-        )
-        .setThumbnail(member.displayAvatarURL())
-        .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
-        .setColor("Yellow")
-    const channel = await client.channels.fetch(joined ? process.env.CHANNEL_JOIN! : process.env.CHANNEL_LEAVE!) as TextChannel;
-    return channel.send({embeds: [embed]});
+    let isSetup = true;
+    const channel = await member.guild.channels.fetch(joined ? client.cache.get(member.guild.id)!.welcome! : client.cache.get(member.guild.id)!.leave!).catch(async () => {
+        await channelsync.deleteChannel(joined, !joined, member.guild.id);
+        isSetup = false
+    }) as GuildTextBasedChannel;
+    if (isSetup) {
+        const embed = new EmbedBuilder()
+            .setTitle(joined ? `${member.user.tag} joined!` : `${member.user.tag} left!`)
+            .setDescription(`**Invited by**: Himself\n**Account create**: <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>\n\n_His invitation is invalid!_`)
+            .setThumbnail(member.displayAvatarURL())
+            .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+            .setColor("Yellow")
+        return channel.send({embeds: [embed]});
+    }
 }
