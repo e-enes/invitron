76951d8894d06e604aafeec9a074bb5fedd84194
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:13:52 +0200

    Update invites.ts

--- commands/invites.ts
+++ commands/invites.ts
@@ -9,6 +9,7 @@ import MyClient from "../ts/class/MyClient";
 import InviteStats from "../ts/interface/InviteStats";
 import invitesync from "../utils/invitesync";
 import undefMember from "../security/undefMember";
+import config from "../config";
 
 export default {
     name: "invites",
@@ -27,32 +28,31 @@ export default {
         if (member === undefined) return undefMember(interaction, client);
 
         try {
-            const invites: InviteStats = await invitesync.getInvites(member.id, interaction.commandGuildId!);
+            const invites: InviteStats = await invitesync.getInvites(member.user.id, interaction.guildId!);
 
             const embed = new EmbedBuilder()
                 .setTitle("Invitation Stats")
-                .setDescription(
-                    `${interaction.member!.user.id === member.user.id ?
-                        `Congratulations, **${member.user.tag}**! You have **${invites.invites}** invitations. \n\n_Here's a breakdown of your invitations:_\n\n**${invites.total}** Regular Invitations\n**${invites.leaves}** Left Invitations\n**${invites.bonus}** Bonus Invitations` :
-                        `**${member.user.tag}** has **${invites.invites}** invitations. \n\n_Here's a breakdown of their invitations:_\n\n**${invites.total}** Regular Invitations\n**${invites.leaves}** Left Invitations\n**${invites.bonus}** Bonus Invitations`
-                    }`
-                )
-                .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
+                .setDescription(`
+                    ${interaction.member!.user.id === member.user.id ?
+                    `Congratulations, **${member.user.tag}**! You have **${invites.invites}** invitations. \n\n_Here's a breakdown of your invitations:_\n\n**${invites.total}** Regular Invitations\n**${invites.leaves}** Left Invitations\n**${invites.bonus}** Bonus Invitations` :
+                    `**${member.user.tag}** has **${invites.invites}** invitations. \n\n_Here's a breakdown of their invitations:_\n\n**${invites.total}** Regular Invitations\n**${invites.leaves}** Left Invitations\n**${invites.bonus}** Bonus Invitations`}
+                `)
+                .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
                 .setColor("DarkGreen")
             return interaction.editReply({embeds: [embed]});
         } catch (error) {
             const embed = new EmbedBuilder()
                 .setTitle("Error!")
-                .setDescription(
-                    `
+                .setDescription(`
                     ${interaction.member!.user.id === member.user.id ?
-                        `Unable to retrieve your invitations, **${member.user.tag}**.` :
-                        `Unable to retrieve invitations from **${member.user.tag}** for **${interaction.member!.user.username + "#" + interaction.member!.user.discriminator}**.`
-                    }
-                    `
-                )
-                .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
-                .setColor("DarkRed")
+                    `Unable to retrieve your invitations, **${member.user.tag}**.` :
+                    `Unable to retrieve invitations from **${member.user.tag}** for **${interaction.member!.user.username + "#" + interaction.member!.user.discriminator}**.`}
+                `)
+                .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                .setColor("Red")
+            config.handleError ?
+                embed.addFields({name: "Console", value: error as string}) :
+                console.error(error);
             return interaction.editReply({embeds: [embed]});
         }
     }
