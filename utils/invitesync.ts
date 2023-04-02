24416448d01d99484683f95012547c59d7b356e9
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:02:42 +0200

    Update invitesync.ts

--- utils/invitesync.ts
+++ utils/invitesync.ts
@@ -7,10 +7,10 @@ import {Snowflake} from "discord.js";
 export default {
     async getInviter(user: Snowflake, guild: Snowflake): Promise<Snowflake> {
         return new Promise<Snowflake>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`user\` = \"${user}\" AND \`guild\` = \"${guild}\" AND \`inactive\` = \"false\"`, (error: Error, results: Array<InviteResults> | undefined) => {
+            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`user\` = "${user}" AND \`guild\` = "${guild}" AND \`inactive\` = "0"`, (error: Error, results: Array<InviteResults> | undefined) => {
                 if (error) reject(error.message);
                 if (results && results[0]) resolve(results[0].inviter);
-                reject(new Error("No results found."));
+                reject(new Error("No inviter found"));
             });
         });
     },
@@ -35,27 +35,27 @@ export default {
     },
     async setInviter(user: Snowflake, inviter: Snowflake, guild: Snowflake): Promise<undefined> {
         return new Promise<undefined>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`leaves\` WHERE \`user\` = "${user}" AND \`guild\` = "${guild}" AND \`inactive\` = "false"`, (error: Error, results: Array<InviteResults>) => {
+            connection.mysql.query(`SELECT * FROM \`leaves\` WHERE \`user\` = "${user}" AND \`guild\` = "${guild}" AND \`inactive\` = "0"`, (error: Error, results: Array<InviteResults>) => {
                 if (error) reject(error.message);
-                if (results[0]) {
-                    connection.mysql.query(`UPDATE \`leaves\` SET \`inactive\` = "true" WHERE \`id\` = "${results[0].id}"`, (error1: Error) => {
+                if (results && results[0]) {
+                    connection.mysql.query(`UPDATE \`leaves\` SET \`inactive\` = "1" WHERE \`id\` = "${results[0].id}"`, (error1: Error) => {
                         if (error1) reject(error1.message);
-                    });
-                    connection.mysql.query(`INSERT INTO \`invites\` (\`user\`, \`guild\`, \`inviter\`, \`inactive\`) VALUES ("${user}", "${guild}", "${inviter}", "false")`, (error1: Error) => {
-                        if (error1) reject(error1.message);
-                        resolve(undefined);
                     });
                 }
+                connection.mysql.query(`INSERT INTO \`invites\` (\`user\`, \`guild\`, \`inviter\`, \`inactive\`) VALUES ("${user}", "${guild}", "${inviter}", "0")`, (error1: Error) => {
+                    if (error1) reject(error1.message);
+                    resolve(undefined);
+                });
             });
         });
     },
     async clearInvites(inviter: Snowflake, guild: Snowflake): Promise<undefined> {
         return new Promise<undefined>((resolve, reject) => {
-            connection.mysql.query(`DELETE FROM \`invites\` WHERE \`inviter\` = \"${inviter}\" AND \`guild\` = \"${guild}\"`, (error: Error) => {
+            connection.mysql.query(`DELETE FROM \`invites\` WHERE \`inviter\` = "${inviter}" AND \`guild\` = "${guild}"`, (error: Error) => {
                 if (error) reject(error.message);
-                connection.mysql.query(`DELETE FROM \`leaves\` WHERE \`inviter\` = \"${inviter}\" AND \`guild\` = \"${guild}\"`, (error1: Error) => {
+                connection.mysql.query(`DELETE FROM \`leaves\` WHERE \`inviter\` = "${inviter}" AND \`guild\` = "${guild}"`, (error1: Error) => {
                     if (error1) reject(error1.message);
-                    connection.mysql.query(`DELETE FROM \`bonus\` WHERE \`user\` = \"${inviter}\" AND \`guild\` = \"${guild}\"`, (error2: Error) => {
+                    connection.mysql.query(`DELETE FROM \`bonus\` WHERE \`user\` = "${inviter}" AND \`guild\` = "${guild}"`, (error2: Error) => {
                         if (error2) reject(error2.message);
                         resolve(undefined);
                     });
@@ -65,14 +65,14 @@ export default {
     },
     async removeInvite(user: Snowflake, inviter: Snowflake, guild: Snowflake): Promise<undefined> {
         return new Promise<undefined>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`user\` = \"${user}\" AND \`guild\` = \"${guild}\" AND \`inactive\` = \"false\"`, (error: Error, results: Array<InviteResults>) => {
+            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`user\` = "${user}" AND \`guild\` = "${guild}" AND \`inactive\` = "0"`, (error: Error, results: Array<InviteResults>) => {
                 if (error) reject(error.message);
                 if (results[0]) {
-                    connection.mysql.query(`UPDATE \`invites\` SET \`inactive\` = \"true\" WHERE \`id\` = \"${results[0].id}\"`, (error1: Error) => {
+                    connection.mysql.query(`UPDATE \`invites\` SET \`inactive\` = "1" WHERE \`id\` = "${results[0].id}"`, (error1: Error) => {
                         if (error1) reject(error1.message);
                     });
                 }
-                connection.mysql.query(`INSERT INTO \`leaves\` (\`user\`, \`guild\`, \`inviter\`, \`inactive\`) VALUES (\"${user}\", \"${guild}\", \"${inviter}\", \"false\")`, (error1: Error) => {
+                connection.mysql.query(`INSERT INTO \`leaves\` (\`user\`, \`guild\`, \`inviter\`, \`inactive\`) VALUES ("${user}", "${guild}", "${inviter}", "0")`, (error1: Error) => {
                     if (error1) reject(error1.message);
                     resolve(undefined);
                 });
@@ -81,7 +81,7 @@ export default {
     },
     async bonusInvites(user: Snowflake, guild: Snowflake, amount: number): Promise<undefined> {
         return new Promise<undefined>((resolve, reject) => {
-            connection.mysql.query(`INSERT INTO \`bonus\` (\`user\`, \`guild\`, \`bonus\`) VALUES (\"${user}\", \"${guild}\", \"${amount}\")`, (error: Error) => {
+            connection.mysql.query(`INSERT INTO \`bonus\` (\`user\`, \`guild\`, \`bonus\`) VALUES ("${user}", "${guild}", "${amount}")`, (error: Error) => {
                 if (error) reject(error.message);
                 resolve(undefined);
             });
@@ -89,11 +89,11 @@ export default {
     },
     async leaderboard(guild: Snowflake): Promise<{ [key: Snowflake]: number }> {
         return new Promise<{ [key: Snowflake]: number }>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`guild\` = \"${guild}\"`, (error: Error, results: Array<InviteResults>) => {
+            connection.mysql.query(`SELECT * FROM \`invites\` WHERE \`guild\` = "${guild}"`, (error: Error, results: Array<InviteResults>) => {
                 if (error) reject(error.message);
-                connection.mysql.query(`SELECT * FROM \`leaves\` WHERE \`guild\` = \"${guild}\"`, (error1: Error, results1: Array<InviteResults>) => {
+                connection.mysql.query(`SELECT * FROM \`leaves\` WHERE \`guild\` = "${guild}"`, (error1: Error, results1: Array<InviteResults>) => {
                     if (error1) reject(error1.message);
-                    connection.mysql.query(`SELECT * FROM \`bonus\` WHERE \`guild\` = \"${guild}\"`, (error2: Error, results2: Array<BonusResults>) => {
+                    connection.mysql.query(`SELECT * FROM \`bonus\` WHERE \`guild\` = "${guild}"`, (error2: Error, results2: Array<BonusResults>) => {
                         if (error2) reject(error2.message);
                         const invites: { [key: Snowflake]: number } = {};
                         for (let i = 0; i < results.length; i++) {
@@ -114,4 +114,4 @@ export default {
             });
         });
     }
-};
+}
