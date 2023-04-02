52935a6a6b426a59cc6ee57effd36d124cb8b043
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:12:36 +0200

    Update noDMCommand.ts

--- security/noDMCommand.ts
+++ security/noDMCommand.ts
@@ -2,12 +2,13 @@ import {
     EmbedBuilder, User
 } from "discord.js";
 import MyClient from "../ts/class/MyClient";
+import config from "../config";
 
 export default async function noDMCommand(member: User, client: MyClient) {
     const embed = new EmbedBuilder()
         .setTitle("Error!")
         .setDescription(`**${member.tag}** direct **message commands** are **not** allowed.`)
-        .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
-        .setColor("DarkRed")
+        .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+        .setColor("Red")
     return member.send({embeds: [embed]});
 }
