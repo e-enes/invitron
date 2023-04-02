6eaaeb264b8d4ab802aacf9428553c97f82018bf
Enes <125091714+enes-th@users.noreply.github.com>
2023-04-02 15:13:34 +0200

    Update add-invites.ts

--- commands/add-invites.ts
+++ commands/add-invites.ts
@@ -1,5 +1,6 @@
 import {
-    ApplicationCommandOptionType, ApplicationCommandType,
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
     CommandInteraction,
     EmbedBuilder,
     GuildMember,
@@ -11,6 +12,7 @@ import invitesync from "../utils/invitesync";
 import undefMember from "../security/undefMember";
 import noPermission from "../security/noPermission";
 import invalidBonus from "../security/invalidBonus";
+import config from "../config";
 
 export default {
     name: "add-invites",
@@ -41,28 +43,25 @@ export default {
         if (bonus <= 0 || bonus >= 2147483647) return invalidBonus(interaction, user, client);
 
         try {
-            const invites: InviteStats = await invitesync.getInvites(member.user.id, interaction.commandGuildId!);
+            const invites: InviteStats = await invitesync.getInvites(member.user.id, interaction.guildId!);
             if (invites.bonus + bonus >= 2147483647) {
                 const embed = new EmbedBuilder()
                     .setTitle("Error!")
-                    .setDescription(`**${user.user.tag}** was unable to add invitations. \n\nThe total number of invitations has exceeded the maximum limit.`)
-                    .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
-                    .setColor("DarkRed");
+                    .setDescription(`**${user.user.tag}** was unable to add invitations.\n\nThe total number of invitations has exceeded the maximum limit.`)
+                    .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                    .setColor("Red");
                 return interaction.editReply({embeds: [embed]});
             }
 
-            await invitesync.bonusInvites(member.user.id, interaction.commandGuildId!, bonus);
+            await invitesync.bonusInvites(member.user.id, interaction.guildId!, bonus);
             const embed = new EmbedBuilder()
                 .setTitle("Add Invitation")
-                .setDescription(
-                    `
+                .setDescription(`
                     ${user.user.id === member.user.id ?
-                        `**${member.user.tag}** you've earned **${bonus}** invitations!` :
-                        `**${user.user.tag}** has added **${bonus}** invitations for **${member.user.tag}**`
-                    }
-                    `
-                )
-                .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
+                    `**${member.user.tag}** you've earned **${bonus}** invitations!` :
+                    `**${user.user.tag}** has added **${bonus}** invitations for **${member.user.tag}**`}
+                `)
+                .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
                 .setColor("DarkGreen")
             return interaction.editReply({embeds: [embed]});
         } catch (error) {
@@ -70,8 +69,11 @@ export default {
                 .setTitle("Error!")
                 .setDescription(`**${user.user.tag}** was unable **to add invitations**.`)
                 .setThumbnail(member.displayAvatarURL())
-                .setFooter({text: "Powered by Sene", iconURL: client.user!.displayAvatarURL()})
-                .setColor("DarkRed")
+                .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                .setColor("Red")
+            config.handleError ?
+                embed.addFields({name: "Console", value: error as string}) :
+                console.error(error);
             return interaction.editReply({embeds: [embed]});
         }
     }
