cbb7436612fcf41111d607fbf8d4c804398b7545
Enes T <125091714+enes-th@users.noreply.github.com>
2023-04-16 15:45:10 +0200

    Update guildMemberAdd.ts

--- events/guildMemberAdd.ts
+++ events/guildMemberAdd.ts
@@ -1,12 +1,17 @@
-import {Events, EmbedBuilder, GuildMember, GuildTextBasedChannel} from "discord.js";
-import MyClient from "../ts/class/MyClient";
-import antiBot from "../security/antiBot";
-import inviteHimself from "../security/inviteHimself";
-import youngAccount from "../security/youngAccount";
-import invitesync from "../utils/invitesync";
-import InviteStats from "../ts/interface/InviteStats";
+import {
+    Events,
+    EmbedBuilder,
+    GuildMember,
+    GuildTextBasedChannel
+} from "discord.js";
+import MyClient from "../lib/types/class/MyClient";
+import antiBot from "../lib/utils/antiBot";
+import inviteHimself from "../lib/utils/inviteHimself";
+import youngAccount from "../lib/utils/youngAccount";
+import inviteSync from "../lib/sync/invite";
+import InviteStats from "../lib/types/interface/InviteStats";
 import config from "../config";
-import channelsync from "../utils/channelsync";
+import channelSync from "../lib/sync/channel";
 
 export default {
     once: false,
@@ -14,36 +19,43 @@ export default {
     async execute(member: GuildMember, client: MyClient) {
         if (member.user.bot) return antiBot(member, client);
 
-        let isSetup = true;
-        const channel = await member.guild.channels.fetch(client.cache.get(member.guild.id)!.welcome!).catch(async () => {
-            await channelsync.deleteChannel(true, false, member.guild.id);
-            isSetup = false
-        }) as GuildTextBasedChannel;
+        const channelId = client.cache.channels.get(member.guild.id)?.welcome;
+        let isSetup = channelId !== undefined;
+        let channel: GuildTextBasedChannel | undefined = undefined;
+
+        if (isSetup) {
+            try {
+                channel = await member.guild.channels.fetch(channelId!) as GuildTextBasedChannel;
+            } catch {
+                await channelSync.deleteChannel(true, false, member.guild.id);
+                isSetup = false;
+            }
+        }
 
         member.guild.invites.fetch()
             .then(async (guildInvites) => {
-                const invites = client.invites.get(member.guild.id)!;
+                const invites = client.cache.invites.get(member.guild.id)!;
                 const invite = guildInvites.find((inv) => invites?.get(inv.code)! < inv.uses!)!;
 
                 await guildInvites.each((inv) => invites.set(inv.code, inv.uses!));
-                await client.invites.set(member.guild.id, invites);
+                await client.cache.invites.set(member.guild.id, invites);
 
                 try {
                     const inviter = await invite.inviter!;
                     if (inviter.id === member.user.id) return inviteHimself(member, client, true);
                     if (Date.now() - member.user.createdTimestamp < 1000 * 60 * 60 * 24 * 10) return youngAccount(member, inviter, client, true);
 
-                    await invitesync.setInviter(member.user.id, inviter.id, member.guild.id);
+                    await inviteSync.setInviter(member.user.id, inviter.id, member.guild.id);
 
                     if (isSetup) {
-                        const invites: InviteStats = await invitesync.getInvites(inviter.id, member.guild.id);
+                        const invites: InviteStats = await inviteSync.getInvites(inviter.id, member.guild.id);
                         const embed = new EmbedBuilder()
                             .setTitle(`${member.user.tag} joined!`)
                             .setDescription(`**Invited by**: ${inviter.tag}\n**Who now has**: ${invites.invites} invitations\n\n**Account create**: <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`)
                             .setThumbnail(member.displayAvatarURL())
                             .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
                             .setColor("DarkGreen")
-                        return channel.send({embeds: [embed]});
+                        return channel!.send({embeds: [embed]});
                     }
                 } catch (error) {
                     if (isSetup) {
@@ -53,12 +65,9 @@ export default {
                             .setThumbnail(member.displayAvatarURL())
                             .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
                             .setColor("Red")
-                        config.handleError ?
-                            embed.addFields({name: "Console", value: error as string}) :
-                            console.error(error);
-                        return channel.send({embeds: [embed]});
+                        if (config.handleError) embed.addFields({name: "Console", value: error as string})
+                        return channel!.send({embeds: [embed]});
                     }
-                    console.error(error);
                 }
             });
     }
