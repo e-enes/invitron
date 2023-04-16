03a481418e8195e7a55af263d02d13cabdf62765
Enes T <125091714+enes-th@users.noreply.github.com>
2023-04-16 15:44:56 +0200

    Update guildDelete.ts

--- events/guildDelete.ts
+++ events/guildDelete.ts
@@ -1,10 +1,14 @@
-import {Events, Guild} from "discord.js";
-import MyClient from "../ts/class/MyClient";
+import {
+    Events,
+    Guild
+} from "discord.js";
+import MyClient from "../lib/types/class/MyClient";
 
 export default {
     once: false,
     name: Events.GuildDelete,
     async execute(guild: Guild, client: MyClient) {
-        client.invites.delete(guild.id);
+        client.cache.invites.delete(guild.id);
+        client.cache.channels.delete(guild.id);
     }
 }
