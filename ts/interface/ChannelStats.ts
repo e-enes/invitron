894e40a3e66540ad7fbcb45a84cab827028edb03
Enes T <125091714+enes-th@users.noreply.github.com>
2023-04-16 15:39:40 +0200

    Delete ts directory

--- ts/interface/ChannelStats.ts
+++
@@ -1,7 +0,0 @@
-import {Snowflake} from "discord.js";
-
-export default interface ChannelStats {
-    welcome: Snowflake | undefined;
-    leave: Snowflake | undefined;
-    log: Snowflake | undefined
-}
--- ts/interface/InviteResults.ts
+++
@@ -1,9 +0,0 @@
-import {Snowflake} from "discord.js";
-
-export default interface InviteResults {
-    id: number,
-    user: Snowflake,
-    guild: Snowflake,
-    inviter: Snowflake,
-    inactive: boolean
-}
