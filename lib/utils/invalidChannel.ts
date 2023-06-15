287e6d2ea08e1f516fad1d3873a97ab10acc6ab0
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:58:05 +0200

    Update invalidChannel.ts

--- lib/utils/invalidChannel.ts
+++ lib/utils/invalidChannel.ts
@@ -9,8 +9,8 @@ import config from "../../config";
 export default async function invalidChannel(interaction: CommandInteraction | ButtonInteraction, client: MyClient) {
     const embed = new EmbedBuilder()
         .setTitle("Error!")
-        .setDescription(`**${interaction.member!.user.username + interaction.member!.user.discriminator}** please choose a **text** channel.`)
-        .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+        .setDescription(`**${interaction.member}** please choose a **text** channel.`)
+        .setFooter({ text: config.message.footer, iconURL: client.user!.displayAvatarURL() })
         .setColor("Red")
-    return interaction.editReply({embeds: [embed]});
+    return interaction.editReply({ embeds: [embed] });
 }
