d11a5f441b45af3c2440880bfd6665efa2c676c3
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:51:35 +0200

    Update guildDelete.ts

--- events/guildDelete.ts
+++ events/guildDelete.ts
@@ -9,6 +9,7 @@ export default {
     name: Events.GuildDelete,
     async execute(guild: Guild, client: MyClient) {
         client.cache.invites.delete(guild.id);
+        client.cache.links.delete(guild.id);
         client.cache.channels.delete(guild.id);
     }
 }
