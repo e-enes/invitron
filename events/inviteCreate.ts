80fefb9aa1883e0f8545b8cc6ca95cc0d58558b3
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:52:53 +0200

    Update inviteCreate.ts

--- events/inviteCreate.ts
+++ events/inviteCreate.ts
@@ -8,6 +8,11 @@ export default {
     once: false,
     name: Events.InviteCreate,
     async execute(invite: Invite, client: MyClient) {
-        await client.cache.invites.get(invite.guild?.id!)?.set(invite.code, invite.uses!);
+        if (invite.inviter?.bot) return;
+
+        client.cache.invites.get(invite.guild?.id!)!.set(invite.code, {
+            uses: invite.uses!,
+            memberId: invite.inviter?.id!
+        });
     }
 }
