b2d29342b4507d6bc27901a454cef245df928c9e
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:54:15 +0200

    Update channel.ts

--- lib/sync/channel.ts
+++ lib/sync/channel.ts
@@ -1,69 +1,123 @@
-import {Snowflake} from "discord.js";
+import { Snowflake } from "discord.js";
 import connection from "../../database/connection";
-import ChannelResults from "../types/interface/ChannelResults";
-import ChannelStats from "../types/interface/ChannelStats";
+import { ChannelResults, ChannelStats } from "../types/interface/channel";
+import { MysqlError } from "mysql";
 
 export default {
-    async setGuild(guild: Snowflake) {
+    /**
+     *
+     * @function
+     * @param {Snowflake} guild
+     * @returns {void}
+     * @throws {MysqlError}
+     */
+    add(guild: Snowflake): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`channels\` WHERE \`guild\` = "${guild}"`, (error: Error, results: Array<ChannelResults>) => {
-                if (error) reject(error.message);
+            const queries = [
+                `SELECT * FROM channels WHERE guild = ?`,
+                `INSERT INTO channels (setup, guild) VALUES (0, ?)`,
+            ];
+
+            connection.mysql.query(queries[0], [guild], (error: MysqlError | null, results: Array<ChannelResults>) => {
+                if (error) reject(error);
                 if (results[0]) resolve();
-                connection.mysql.query(`INSERT INTO \`channels\` (\`setup\`, \`guild\`, \`welcome\`, \`leave\`, \`log\`) VALUES ("0", "${guild}", "undefined", "undefined", "undefined")`, (error1: Error) => {
-                    if (error1) reject(error1.message);
+                connection.mysql.query(queries[1], [guild], (error1: MysqlError | null) => {
+                    if (error1) reject(error1);
                     resolve();
                 });
             });
         });
     },
-    async setup(welcome: Snowflake, leave: Snowflake, log: Snowflake, guild: Snowflake) {
+
+    /**
+     *
+     * @function
+     * @param {Snowflake} welcome
+     * @param {Snowflake} leave
+     * @param {Snowflake} log
+     * @param {Snowflake} guild
+     * @returns {void}
+     * @throws {MysqlError}
+     */
+    set(welcome: Snowflake, leave: Snowflake, log: Snowflake, guild: Snowflake): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`channels\` WHERE \`guild\` = "${guild}"`, async (error: Error, results: Array<ChannelResults>) => {
-                if (error) reject(error.message);
+            const queries = [
+                `SELECT * FROM channels WHERE guild = ?`,
+                `UPDATE channels SET setup = 1 AND welcome = ? AND \`leave\` = ? AND log = ? WHERE id = ?`,
+                `INSERT INTO channels (setup, guild, welcome, \`leave\`, log) VALUES (1, ?, ?, ?, ?)`
+            ];
+
+            connection.mysql.query(queries[0], [guild], (error: MysqlError | null, results: Array<ChannelResults>) => {
+                if (error) reject(error);
                 if (results && results[0]) {
-                    connection.mysql.query(`UPDATE \`channels\` SET \`setup\` = "1" AND \`welcome\` = "${welcome}" AND \`leave\` = "${leave}" AND \`log\` = "${log}" WHERE \`id\` = "${results[0].id}"`, (error1: Error) => {
-                        if (error1) reject(error1.message);
+                    connection.mysql.query(queries[1], [welcome, leave, log, results[0].id], (error1: MysqlError | null) => {
+                        if (error1) reject(error1);
                         resolve();
                     });
                 } else {
-                    connection.mysql.query(`INSERT INTO \`channels\` (\`setup\`, \`guild\`, \`welcome\`, \`leave\`, \`log\`) VALUES ("1", "${guild}", "${welcome}", "${leave}", "${log}")`, (error1: Error) => {
-                        if (error1) reject(error1.message);
+                    connection.mysql.query(queries[2], [guild, welcome, leave, log], (error1: MysqlError | null) => {
+                        if (error1) reject(error1);
                         resolve();
                     });
                 }
             });
         });
     },
-    async getChannels(guild: Snowflake) {
+
+    /**
+     *
+     * @function
+     * @param {Snowflake} guild
+     * @returns {void}
+     * @throws {MysqlError}
+     */
+    get(guild: Snowflake): Promise<ChannelStats> {
         return new Promise<ChannelStats>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`channels\` WHERE \`guild\` = "${guild}"`, (error: Error, results: Array<ChannelResults>) => {
-                if (error) reject(error.message);
+            const query = `
+                SELECT * FROM channels WHERE guild = ?
+            `;
+
+            connection.mysql.query(query, [guild], (error: MysqlError | null, results: Array<ChannelResults>) => {
+                if (error) reject(error);
                 if (results && results[0]) {
                     resolve({
                         welcome: results[0].welcome ?? undefined,
                         leave: results[0].leave ?? undefined,
                         log: results[0].log ?? undefined
                     });
-                } else {
-                    resolve({
-                        welcome: undefined,
-                        leave: undefined,
-                        log: undefined
-                    });
-                }
+                } else resolve({
+                    welcome: undefined,
+                    leave: undefined,
+                    log: undefined
+                });
             });
         });
     },
-    async deleteChannel(welcome: boolean, leave: boolean, guild: Snowflake) {
+
+    /**
+     *
+     * @function
+     * @param {Snowflake} welcome
+     * @param {Snowflake} leave
+     * @param {Snowflake} guild
+     * @returns {void}
+     * @throws {MysqlError}
+     */
+    del(welcome: boolean, leave: boolean, guild: Snowflake): Promise<void> {
         return new Promise<void>((resolve, reject) => {
-            connection.mysql.query(`SELECT * FROM \`channels\` WHERE \`guild\` = "${guild}"`, (error: Error, results: Array<ChannelResults>) => {
-                if (error) reject(error.message);
+            const queries = [
+                `SELECT * FROM channels WHERE guild = ?`,
+                `UPDATE channels SET setup = 0 ${welcome ? `AND welcome = undefined` : leave ? `AND \`leave\` = undefined` : `AND log = undefined`} WHERE id = ?`
+            ];
+
+            connection.mysql.query(queries[0], [guild], (error: MysqlError | null, results: Array<ChannelResults>) => {
+                if (error) reject(error);
                 if (results[0]) {
-                    connection.mysql.query(`UPDATE \`channels\` SET \`setup\` = "0" ${welcome ? `AND \`welcome\` = "undefined"` : leave ? `AND \`leave\` = "undefined"` : `AND \`log\` = "undefined"`} WHERE \`id\` = "${results[0].id}"`, (error1: Error) => {
-                        if (error1) reject(error1.message);
+                    connection.mysql.query(queries[1], [results[0].id], (error1: MysqlError | null) => {
+                        if (error1) reject(error1);
                         resolve();
                     });
-                }
+                } else reject(new Error("No channels found."))
             });
         });
     }
