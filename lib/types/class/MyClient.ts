ab03e45db02427c364b4434cc3afb0ecbab36fac
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:55:42 +0200

    Update MyClient.ts

--- lib/types/class/MyClient.ts
+++ lib/types/class/MyClient.ts
@@ -1,14 +1,16 @@
 import {Client} from "discord.js";
 import Command from "../interface/Command";
-import ChannelStats from "../interface/ChannelStats";
+import {ChannelStats} from "../interface/channel";
 
 export default class MyClient extends Client {
     cache: {
-        invites: Map<string, Map<string, number>>,
+        invites: Map<string, Map<string, { uses: number, memberId: string }>>,
+        links: Map<string, Map<string, string>>,
         register: Command[],
         channels: Map<string, ChannelStats>
     } = {
         invites: new Map(),
+        links: new Map(),
         register: [],
         channels: new Map()
     };
