b0ba4321ce221022add6801c3df9107dd7a48e23
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:58:47 +0200

    Update noPermission.ts

--- lib/utils/noPermission.ts
+++ lib/utils/noPermission.ts
@@ -10,8 +10,8 @@ import config from "../../config";
 export default async function noPermission(interaction: CommandInteraction | ButtonInteraction, member: GuildMember, client: MyClient) {
     const embed = new EmbedBuilder()
         .setTitle("Error!")
-        .setDescription(`**${member.user.tag}** you **do not have permission** to use this command!`)
-        .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+        .setDescription(`**${member}** you **do not have permission** to use this command!`)
+        .setFooter({ text: config.message.footer, iconURL: client.user!.displayAvatarURL() })
         .setColor("Red")
-    return interaction.editReply({embeds: [embed]});
+    return interaction.editReply({ embeds: [embed] });
 }
