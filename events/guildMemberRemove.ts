3a54b5eaa9d5769077923e359f1eba6601445329
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:52:15 +0200

    Update guildMemberRemove.ts

--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -7,11 +7,11 @@ import {
 } from "discord.js";
 import MyClient from "../lib/types/class/MyClient";
 import inviteSync from "../lib/sync/invite";
-import inviteHimself from "../lib/utils/inviteHimself";
-import youngAccount from "../lib/utils/youngAccount";
-import InviteStats from "../lib/types/interface/InviteStats";
+import { InviteStats } from "../lib/types/interface/invite";
 import config from "../config";
 import channelSync from "../lib/sync/channel";
+import inviteHimself from "../lib/utils/inviteHimself";
+import youngAccount from "../lib/utils/youngAccount";
 
 export default {
     once: false,
@@ -25,7 +25,7 @@ export default {
             try {
                 channel = await member.guild.channels.fetch(channelId!) as GuildTextBasedChannel;
             } catch {
-                await channelSync.deleteChannel(false, true, member.guild.id);
+                await channelSync.del(false, true, member.guild.id);
                 isSetup = false;
             }
         }
@@ -35,38 +35,39 @@ export default {
                 .setTitle(`${member.user.tag} left!`)
                 .setDescription(`**Bot User**\n**Account create**: <t:${Math.floor(member.user.createdTimestamp) / 1000}:R>`)
                 .setThumbnail(member.displayAvatarURL())
-                .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                .setFooter({ text: config.message.footer, iconURL: client.user!.displayAvatarURL() })
                 .setColor("Yellow")
-            return channel!.send({embeds: [embed]});
+            return channel!.send({ embeds: [embed] });
         }
 
         try {
             const inviter: User = await inviteSync.getInviter(member.user.id, member.guild.id)
                 .then((u) => client.users.fetch(u));
+            await inviteSync.remove(member.user.id, inviter.id, member.guild.id);
+
             if (inviter.id === member.user.id) return inviteHimself(member, client, false);
             if (Date.now() - member.user.createdTimestamp < 1000 * 60 * 60 * 24 * 10) return youngAccount(member, inviter, client, false);
 
-            await inviteSync.removeInvite(member.user.id, inviter.id, member.guild.id);
             if (isSetup) {
                 const invites: InviteStats = await inviteSync.getInvites(inviter.id, member.guild.id);
                 const embed = new EmbedBuilder()
-                    .setTitle(`${member.user.tag} left!`)
-                    .setDescription(`**Invited by**: ${inviter.tag}\n**Who now has**: ${invites.invites} invitations\n\n**Account create**: <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`)
+                    .setTitle(`${member.user.username} left!`)
+                    .setDescription(`**Invited by**: ${inviter.username}\n**Who now has**: ${invites.invites} invitations\n\n**Account create**: <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`)
                     .setThumbnail(member.displayAvatarURL())
-                    .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                    .setFooter({ text: config.message.footer, iconURL: client.user!.displayAvatarURL() })
                     .setColor("Red")
-                return channel!.send({embeds: [embed]});
+                return channel!.send({ embeds: [embed] });
             }
-        } catch (error) {
+        } catch (error: any) {
             if (isSetup) {
                 const embed = new EmbedBuilder()
-                    .setTitle(`${member.user.tag} left!`)
+                    .setTitle(`${member.user.username} left!`)
                     .setDescription(`**Invited by**: Unknow inviter\n**Account create**: <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>\n\n_Maybe he used a temporary or vanity invitation_`)
                     .setThumbnail(member.displayAvatarURL())
-                    .setFooter({text: config.message.footer, iconURL: client.user!.displayAvatarURL()})
+                    .setFooter({ text: config.message.footer, iconURL: client.user!.displayAvatarURL() })
                     .setColor("Red")
-                if (config.handleError) embed.addFields({name: "Console", value: error as string})
-                return channel!.send({embeds: [embed]});
+                if (config.handleError) embed.addFields({ name: "Console", value: error.message })
+                return channel!.send({ embeds: [embed] });
             }
         }
     }
