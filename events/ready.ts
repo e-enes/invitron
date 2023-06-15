89c147d5f223086c12db796abb7309c7f7556e14
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:53:27 +0200

    Update ready.ts

--- events/ready.ts
+++ events/ready.ts
@@ -1,9 +1,11 @@
-import {Events} from "discord.js";
+import { Events } from "discord.js";
 import MyClient from "../lib/types/class/MyClient";
-import {register} from "../lib/sync/slash";
+import { register } from "../lib/sync/slash";
 import channelSync from "../lib/sync/channel";
 import connection from "../database/connection";
 import config from "../config";
+import { LinkStats } from "../lib/types/interface/invite";
+import linkSync from "../lib/sync/link";
 
 export default {
     once: true,
@@ -13,24 +15,43 @@ export default {
             client.cache.register.map((command) => ({
                 name: command.name,
                 description: command.description,
-                options: command.options,
+                options: command.options ?? undefined,
                 type: command.type,
             }))
         );
         await connection.executeQueries();
 
-        client.user!.setActivity({name: config.activity.name, type: config.activity.type});
+        client.user!.setActivity({ name: config.activity.name, type: config.activity.type });
 
         for (let i = 0; i < client.guilds.cache.size; i++) {
             const guild = client.guilds.cache.at(i)!;
 
-            const channels = await channelSync.getChannels(guild.id);
+            const channels = await channelSync.get(guild.id);
             client.cache.channels.set(guild.id, channels);
 
             const guildInvites = await guild.invites.fetch();
-            const code = new Map();
-            guildInvites.each((inv) => code.set(inv.code, inv.uses));
+            const code = new Map<string, { uses: number; memberId: string }>();
+
+            const links = await linkSync.get(guild.id);
+            const link = new Map<string, string>();
+
+            for (const [key, value] of Object.entries(links)) {
+                link.set(key, value);
+            }
+            client.cache.links.set(guild.id, link);
+            const linksMap = await client.cache.links.get(guild.id)!;
+
+            guildInvites.each((inv) => {
+                const invCode = inv.code;
+                const invUses = inv.uses!;
+                const inviterId = linksMap?.has(invCode) ? linksMap.get(invCode)! : inv.inviter?.id!;
+
+                code.set(invCode, { uses: invUses, memberId: inviterId });
+            });
+
             client.cache.invites.set(guild.id, code);
         }
+
+        console.log("login!");
     }
 }
