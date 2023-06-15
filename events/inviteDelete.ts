4e5639913bdb208063d0d15eb8a73342fe0f3511
Enes T <125091714+enes-th@users.noreply.github.com>
2023-06-15 20:53:11 +0200

    Update inviteDelete.ts

--- events/inviteDelete.ts
+++ events/inviteDelete.ts
@@ -3,11 +3,21 @@ import {
     Invite
 } from "discord.js";
 import MyClient from "../lib/types/class/MyClient";
+import linkSync from "../lib/sync/link";
 
 export default {
     once: false,
     name: Events.InviteDelete,
     async execute(invite: Invite, client: MyClient) {
-        await client.cache.invites.get(invite.guild?.id!)?.delete(invite.code);
+        const guildId = invite.guild?.id!;
+        const links = client.cache.links.get(guildId)!
+
+        client.cache.invites.get(guildId)!.delete(invite.code);
+
+        if (links.has(invite.code)) {
+            await linkSync.del(guildId, invite.code);
+            links.delete(invite.code);
+            client.cache.links.set(guildId, links);
+        }
     }
 }
