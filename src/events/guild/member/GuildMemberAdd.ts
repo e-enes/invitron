8619c9b64f3adefdf895a94afe91055f0550351e
Enes Tahiri <125091714+e-enes@users.noreply.github.com>
2024-04-03 14:10:23 +0200

    perf: redundancy removal

--- src/events/guild/member/GuildMemberAdd.ts
+++ src/events/guild/member/GuildMemberAdd.ts
@@ -10,7 +10,7 @@ class GuildMemberAdd extends Listener {
   }
 
   public override async execute(member: GuildMember) {
-    const { database, config } = this.client;
+    const { database, config, utils } = this.client;
 
     const context = await this.getContext(member.guild.id);
 
@@ -181,7 +181,7 @@ class GuildMemberAdd extends Listener {
     )?.[0];
     const invites = preInvites?.valid + preInvites?.bonus;
 
-    await this.updateRole(member.guild.id, inviter, invites);
+    await utils.updateRole(member.guild.id, inviter, invites);
 
     if (usedInvite.source) {
       await context.send?.({
@@ -253,59 +253,6 @@ class GuildMemberAdd extends Listener {
     }
 
     return row;
-  }
-
-  private async updateRole(guildId: string, inviter: GuildMember, invites: number) {
-    const { database } = this.client;
-
-    const data = await database
-      .query(
-        "SELECT R.role_id AS role, R.number_invitations AS requiredInvitations, RC.keep_role AS keepRole, RC.stacked_role AS stackedRole FROM roles R LEFT JOIN roles_configuration RC ON R.guild_id = RC.guild_id WHERE R.guild_id = ? AND R.active = true GROUP BY R.role_id",
-        [guildId]
-      )
-      .catch(() => void 0);
-
-    if (!data || data.length === 0) {
-      return;
-    }
-
-    const { keepRole, stackedRole } = data[0];
-    const currentRoles = inviter.roles.cache;
-
-    let rolesToAdd: string[] = [];
-    let rolesToRemove: string[] = [];
-
-    for (const { role, requiredInvitations } of data) {
-      const hasRole = currentRoles.has(role);
-      const meetsRequirement = invites >= requiredInvitations;
-
-      if (meetsRequirement && !hasRole) {
-        rolesToAdd.push(role);
-      } else if (!meetsRequirement && hasRole && !keepRole) {
-        rolesToRemove.push(role);
-      }
-    }
-
-    if (!stackedRole) {
-      const highestRequiredInvitations = Math.max(...rolesToAdd.map((role) => data.find((r) => r.role === role).requiredInvitations));
-      rolesToAdd = rolesToAdd.filter((role) => data.find((r) => r.role === role).requiredInvitations === highestRequiredInvitations);
-
-      rolesToRemove = rolesToRemove.concat(
-        currentRoles
-          .filter((roleCache) => {
-            const roleData = data.find((r) => r.role === roleCache.id);
-            return roleData && roleData.requiredInvitations < highestRequiredInvitations;
-          })
-          .map((roleCache) => roleCache.id)
-      );
-    }
-
-    rolesToRemove = rolesToRemove.filter((role) => !rolesToAdd.includes(role));
-
-    await Promise.allSettled([
-      ...rolesToAdd.map((role) => inviter.roles.add(role)),
-      ...rolesToRemove.map((role) => inviter.roles.remove(role)),
-    ]);
   }
 }
 
