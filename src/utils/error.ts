66c219e62ebbf2a15f5d118cececb5514a79a9df
Enes Tahiri <125091714+e-enes@users.noreply.github.com>
2024-04-13 19:10:36 +0200

    feat: updated error handler

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -11,31 +11,45 @@ if (webhookId && webhookToken) {
   webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
 }
 
-process.on("uncaughtException", (error) => {
-  webhookClient
-    .send({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("Uncaught Exception")
-          .setDescription(
-            `**Message:** ${error?.message}\n\n**Name:** ${error?.name}\n**Cause:** ${error?.cause}\n**Source:** ${String(error?.cause)}\n\n**Stack:**\n\`${error?.stack}\``
-          )
-          .setColor(config.message.colors.error),
-      ],
-    })
-    ?.catch(() => {
-      console.error(error);
-      process.exit(1);
-    });
+process.on("uncaughtException", async (error) => {
+  if (webhookClient) {
+    await webhookClient
+      .send({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("Uncaught Exception")
+            .setDescription(
+              `**Message:** ${error?.message}\n\n**Name:** ${error?.name}\n**Cause:** ${error?.cause}\n**Source:** ${String(error?.cause)}\n\n**Stack:**\n\`${error?.stack}\``
+            )
+            .setColor(config.message.colors.error),
+        ],
+      })
+      .catch((e) => {
+        console.error("Webhook Error", e);
+        process.exit(1);
+      });
+
+    return;
+  }
+
+  console.error(error);
+  process.exit(1);
 });
 
-process.on("unhandledRejection", (reason) => {
-  webhookClient
-    .send({
-      embeds: [new EmbedBuilder().setTitle("Uncaught Rejection").setDescription(String(reason)).setColor(config.message.colors.error)],
-    })
-    .catch(() => {
-      console.error(reason);
-      process.exit(1);
-    });
+process.on("unhandledRejection", async (reason) => {
+  if (webhookClient) {
+    await webhookClient
+      .send({
+        embeds: [new EmbedBuilder().setTitle("Uncaught Rejection").setDescription(String(reason)).setColor(config.message.colors.error)],
+      })
+      .catch((e) => {
+        console.error("Webhook Error", e);
+        process.exit(1);
+      });
+
+    return;
+  }
+
+  console.error(reason);
+  process.exit(1);
 });
